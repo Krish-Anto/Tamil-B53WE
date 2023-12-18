58a1db8790a555384e67ff146eb0bd7b0a962caa
Krish-Anto <krishnarajan1995@gmail.com>
2023-12-18 08:14:29 +0530

    2nd commit

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+// var container = document.createElement("div")
+// container.className = "container";
+
+// var row = document.createElement("div")
+// row.className = "row";
+
+// var col = document.createElement("div")
+// col.className = "col";
+
+
+// row.append(col);
+// container.append(row);
+// document.body.append(container);
